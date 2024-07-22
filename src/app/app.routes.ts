b72ff16c6f260e8159cd3c7767dbeb92ebd42f3f
Flavio-Lucio <flavioluqueiroz@gmail.com>
2024-07-22 08:02:02 -0300

    Desafio De Programação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PrimeiroComponent } from './primeiro/primeiro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'primeiro',
+        component: PrimeiroComponent
+    },
+    {
+        path: 'segundo',
+        component: PrimeiroComponent
+    }
+];
