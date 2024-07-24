c2e0efaf239c8604888f35af5293f2c6fb5b6cb7
Flavio-Lucio <flavioluqueiroz@gmail.com>
2024-07-24 12:41:21 -0300

    Botões Adicionados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MatToolbarModule
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/primeiro/primeiro.component.ts
+++ src/app/primeiro/primeiro.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {  MatToolbarModule } from '@angular/material/toolbar';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-primeiro',
   standalone: true,
   imports: [
-    MatToolbarModule
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   templateUrl: './primeiro.component.html',
   styleUrl: './primeiro.component.css'
